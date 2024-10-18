77dc8e93bcbbb326eef22d6dca2e75c5206b067a
Julie <sjulia.3009@gmail.com>
2024-10-18 18:39:21 +0300

    Add files via upload

---
+++ module_10_2.py
@@ -0,0 +1,35 @@
+from threading import Thread
+import time
+
+
+class Knight(Thread):
+    def __init__(self, name, power):
+        super().__init__()
+        self.name = name
+        self.power = power
+        self.total_enemies = 100
+
+
+    def run(self):
+        days = 0
+        print(f'{self.name}, на нас напали!')
+        while self.total_enemies > 0:
+            time.sleep(1)
+            days += 1
+            self.total_enemies -= self.power
+            if self.total_enemies < 0:
+                self.total_enemies = 0
+            print(f'{self.name} сражается {days}..., осталось {self.total_enemies} воинов.')
+        print(f'{self.name} одержал победу спустя {days} дней(дня)!')
+
+
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight('Sir Galahad', 20)
+
+first_knight.start()
+second_knight.start()
+
+first_knight.join()
+second_knight.join()
+
+print('Все битвы закончились!')
